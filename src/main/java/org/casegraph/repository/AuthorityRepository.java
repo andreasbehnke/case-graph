97b1db02c5785b3629a7b4c6a34d55d51edd9eae
andreasbehnke <ab@andreasbehnke.com>
2021-01-30 18:29:59 +0100

    initial app creation with jhipster

---
+++ src/main/java/org/casegraph/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.casegraph.repository;
+
+import org.casegraph.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
